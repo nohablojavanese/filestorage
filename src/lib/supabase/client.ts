48a2aa747b566b3f919412a542a6278c399a17ed
jeremyperwira <jeremyperwira@protonmail.com>
2024-07-27 09:11:49 +0700

    feat: dev env

--- src/lib/supabase/client.ts
+++ src/lib/supabase/client.ts
@@ -1,9 +1,8 @@
 import { createBrowserClient } from "@supabase/ssr";
 
 export function createClient() {
-  const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || 'https://zxzxfdgkomtkapzleztk.supabase.co';
-  const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inp4enhmZGdrb210a2FwemxlenRrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjIwMjM1NDEsImV4cCI6MjAzNzU5OTU0MX0.jQPSu_pxv9VBsMZ3Ipr7ZjFqM29NrsMq0oPNSLVqvVs';
-
+  const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
+  const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY 
   if (!supabaseUrl || !supabaseAnonKey) {
     throw new Error('Missing Supabase URL or Anon Key');
   }
